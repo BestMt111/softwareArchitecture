4dad4796afaccdeb5352dde8490b3ed002862e8c
limengtao <limengtao402@bupt.edu.cn>
2023-12-08 21:24:59 +0800

    上传标准测试文件的输出以及空行输出

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -4,10 +4,10 @@ public class Main {
         //创建主题
         KWICSubject kwicSubject = new KWICSubject();
         //创建观察者
-        Input input = new Input("D:\\Lessons\\softwareArch\\project\\file\\test_in.txt");
+        Input input = new Input("D:\\Lessons\\softwareArch\\project\\file\\null_in.txt");
         Shift shift = new Shift(input.getLineTxt());
         Alphabetizer alphabetizer = new Alphabetizer(shift.getKwicList());
-        Output output = new Output(alphabetizer.getKwicList(), "D:\\Lessons\\softwareArch\\project\\file\\test_out.txt");
+        Output output = new Output(alphabetizer.getKwicList(), "D:\\Lessons\\softwareArch\\project\\file\\null_out.txt");
 
         // 将观察者加入主题
         kwicSubject.addObserver(input);
